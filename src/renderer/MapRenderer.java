6cda879dd27e5a6aff606ab3e26ff19a80a7319c
Jijidici <jdefaye.jijidici@gmail.com>
2013-04-11 19:57:38 +0200

    Add the loading of the base sprite

--- src/renderer/MapRenderer.java
+++ src/renderer/MapRenderer.java
@@ -7,8 +7,11 @@ import java.io.IOException;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
+import engine.Base;
+
 public class MapRenderer {
 	private JLabel background;
+	private ImageIcon baseImage;
 	private Container c;
 	private int width;
 	private int height;
@@ -16,9 +19,14 @@ public class MapRenderer {
 	public MapRenderer(Container c, int width, int height){
 		super();
 		this.background = new JLabel();
+		this.baseImage = new ImageIcon();
 		this.c = c;
 		this.width = width;
 		this.height = height;
+	}
+	
+	public void renderABase(Base b){
+		
 	}
 	
 	public void init() throws IOException{
@@ -26,10 +34,15 @@ public class MapRenderer {
 		ImageIcon bgImage = new ImageIcon("./tex/background.jpg");
 		if(bgImage.getImageLoadStatus() != MediaTracker.COMPLETE){
 			throw new IOException();
-		}
-		
+		}		
 		this.background.setBounds(0, 0, this.width, this.height);
 		this.background.setIcon(bgImage);
 		c.add(this.background);
+		
+		//Load the base image
+		this.baseImage = new ImageIcon("./tex/base.png");
+		if(this.baseImage.getImageLoadStatus() != MediaTracker.COMPLETE){
+			throw new IOException();
+		}
 	}
 }
