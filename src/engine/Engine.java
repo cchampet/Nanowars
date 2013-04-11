a2a6b45db06d805b5d1caa9e37eb27c324917b86
Jijidici <jdefaye.jijidici@gmail.com>
2013-04-11 14:41:53 +0200

    Test jiji

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -6,7 +6,7 @@ public class Engine {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Main of Engine");
+		System.out.println("Main of Engine with jiji");
 	}
 
 }
