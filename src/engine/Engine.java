19cbb594f8c11a2e0b3729254feaec6553cad564
Jijidici <jdefaye.jijidici@gmail.com>
2013-04-11 18:01:15 +0200

    Add the loading of an background image

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,16 +1,18 @@
 package engine;
 
+import java.io.IOException;
+
 import renderer.Renderer;
 
 public class Engine {
 	
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		//Create the Renderer
 		Renderer renderer = new Renderer("Nano WAAAARS!!!");
 		renderer.init();
 	}
-
 }
--- src/renderer/Renderer.java
+++ src/renderer/Renderer.java
@@ -1,21 +1,46 @@
 package renderer;
 
+import java.awt.Container;
+import java.awt.MediaTracker;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Renderer {
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 600;
 	private JFrame f;
+	private JLabel background;
 	
 	public Renderer(String frameTitle){
 		super();
 		this.f = new JFrame(frameTitle);
+		this.background = new JLabel("Toto");
 	}
 	
-	public void init(){
+	public void init() throws IOException{
+		//Set up the frame
 		this.f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.f.setSize(WIDTH, HEIGHT);
 		this.f.setResizable(false);
+		this.f.setLayout(null);
+		Container c = this.f.getContentPane();
+		
+		//Set up the background image
+		//Load the background image
+		ImageIcon bgImage = new ImageIcon("./tex/background.jpg");
+		if(bgImage.getImageLoadStatus() != MediaTracker.COMPLETE){
+			throw new IOException();
+		}
+		
+		System.out.println(bgImage.getImageLoadStatus());
+		
+		this.background.setBounds(0, 0, WIDTH, HEIGHT);
+		//this.background.setIcon(bgImage.getImage());
+		c.add(this.background);
+		
 		
 		this.f.setVisible(true);
 	}
