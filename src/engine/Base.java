14bfc30e07ab6ebebf531781c933969e031d7ae0
Jijidici <jdefaye.jijidici@gmail.com>
2013-04-11 19:57:28 +0200

    Add a JLabel to Base Class for its sprite

--- src/engine/Base.java
+++ src/engine/Base.java
@@ -1,14 +1,28 @@
 package engine;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.vecmath.Vector2f;
 
 public class Base {
 	private final Vector2f position;
 	private int capacity;
+	private final JLabel baseSprite;
 	
 	public Base(int posX, int posY, int capacity){
+		super();
 		this.position = new Vector2f(posX, posY);
 		this.capacity = capacity;
+		this.baseSprite = new JLabel();
+	}
+	
+	public void setSprite(ImageIcon img){
+		this.baseSprite.setIcon(img);
+		this.baseSprite.setBounds(0, 0, capacity, capacity);
+	}
+	
+	public JLabel getSprite(){
+		return this.baseSprite;
 	}
 	
 	public int getXCoord(){
