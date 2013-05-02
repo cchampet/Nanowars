26190113f55a20861f89fef4f94495551fc32468
cchampet <cl.champetier@gmail.com>
2013-05-02 17:35:26 +0200

    BaseSprite : select a base. Still the same problem : the JTextField catches mouse event, so it can be hard to select a little base (because of the text).

--- src/renderer/BaseSprite.java
+++ src/renderer/BaseSprite.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JTextField;
 
 import engine.Base;
@@ -16,7 +17,7 @@ import engine.Base;
  *
  */
 @SuppressWarnings("serial")
-public class BaseSprite extends Sprite {
+public class BaseSprite extends Sprite implements MouseListener {
 	/**
 	 * nbAgents is the JTextField which is used to display the nbAgents of the correpsonding base.
 	 */
@@ -31,33 +32,38 @@ public class BaseSprite extends Sprite {
 		this.nbAgents.setEnabled(false);
 		this.nbAgents.setBorder(null);
 		this.nbAgents.setOpaque(false);
+		this.nbAgents.setIgnoreRepaint(false); // for better performence
 		this.add(this.nbAgents, BorderLayout.CENTER);
-		
-		this.addMouseListener(new MouseListener() {
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                System.out.println(":MOUSE_RELEASED_EVENT:");
-            }
-            @Override
-            public void mousePressed(MouseEvent e) {
-                System.out.println(":MOUSE_PRESSED_EVENT:");
-            }
-            @Override
-            public void mouseExited(MouseEvent e) {
-                //System.out.println(":MOUSE_EXITED_EVENT:");
-            }
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                //System.out.println(":MOUSE_ENTER_EVENT:");
-            }
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                //System.out.println(":MOUSE_CLICK_EVENT:");
-            }
-        });
+		this.addMouseListener(this);
 	}
 	
 	public JTextField getNbAgents() {
 		return this.nbAgents;
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		this.setBorder(null);
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		this.setOpaque(true);
+		this.setBackground(new Color(255, 100, 100));
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		this.setOpaque(false);
+		this.setBackground(null); // i don't know why i need to indicate a new background color...
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
 	}
 }
