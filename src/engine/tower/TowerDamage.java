9a6de2926ddab2223bae84c6139fc1cf09ef11d9
Jijidici <jdefaye.jijidici@gmail.com>
2013-05-27 18:55:10 +0200

    Add TowerDamage Effect

--- src/engine/tower/TowerDamage.java
+++ src/engine/tower/TowerDamage.java
@@ -1,14 +1,27 @@
 package engine.tower;
 
+import engine.Unit;
+
 public class TowerDamage extends TowerAttack {
 
 	public TowerDamage(int posX, int posY) {
 		super(posX, posY);
-		// TODO Auto-generated constructor stub
+		this.setDamage(2+2*this.level);
 	}
 	
 	public TowerDamage(Tower other){
 		super(other);
+		this.setDamage(2+2*this.level);
 	}
-
+	
+	@Override
+	public void levelUp(){
+		super.levelUp();
+		this.setDamage(2+2*this.level);
+	}
+	
+	@Override
+	public void applyEffect(Unit targetedUnit){
+		targetedUnit.reduceNbAgents(this.getDamage());
+	}
 }
